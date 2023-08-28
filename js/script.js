9a5105296b5b8e36ea9e2eb5259a1c81413c23ae
Gurushesh-Metapercept <gurushesh.p@metapercept.com>
2023-08-28 16:04:10 +0530

    download button added

--- js/script.js
+++ js/script.js
@@ -1,69 +1,10 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const searchInput = document.getElementById('search-input');
-//   const searchResults = document.getElementById('search-results');
-//   const noserach = document.getElementById('noserach');
-
-//   const client = algoliasearch('QX9MQYMQ4D', 'edc43cd3cc2ceddc90b7eb276b3ccf1e');
   
-//   searchInput.addEventListener('input', async (event) => {
-//     const query = event.target.value;
-  
-//     if(query.length === 0){
-//       nosearch.style.display = 'block';
-//     }
-
-//     if (query.length > 0) {
-//       nosearch.style.display = 'none';
-//       const index = client.initIndex('output-frontend'); // Replace with your actual index name
-//       const { hits } = await index.search(query);
-  
-//       // Clear previous results
-//       searchResults.innerHTML = '';
-
-//      // Function to decode HTML entities
-//         function decodeHtmlEntities(text) {
-//           const parser = new DOMParser();
-//           const decodedString = parser.parseFromString(text, 'text/html').body.textContent;
-//           return decodedString;
-//         }
-
-//         hits.forEach((hit) => { 
-//           console.log(hit)
-//           // Create the outer card container
-//           const resultItem = document.createElement('div');
-//           resultItem.classList.add('card');
-
-//           // Create the card body container
-//           const cardBody = document.createElement('div');
-//           cardBody.classList.add('card-body');
-          
-//           // Create the anchor link
-//           const anchorLink = document.createElement('a');
-//           anchorLink.href = hit.url;
-//           anchorLink.textContent = decodeHtmlEntities(hit._highlightResult.hierarchy.lvl1.value); // Decode HTML entities
-
-//           // Append the anchor link to the card body
-//           cardBody.appendChild(anchorLink);
-
-//           // Append the card body to the card container
-//           resultItem.appendChild(cardBody);
-
-//           // Append the whole card to the search results
-//           searchResults.appendChild(resultItem);
-//         });
-
-      
-      
-//     } else {
-//       searchResults.innerHTML = ''; // Clear results when query is empty
-//     }
-//   });
-//   })
-
+  // ============================================================ Algolia and Search Modal
   document.addEventListener("DOMContentLoaded", () => {
     const searchInput = document.getElementById('search-input');
     const searchResults = document.getElementById('search-results');
     const nosearch = document.getElementById('nosearch');
+    const headerSearchBox = document.getElementById('headerSearchBox');
   
     const client = algoliasearch('QX9MQYMQ4D', 'edc43cd3cc2ceddc90b7eb276b3ccf1e');
     const indexName = "output-frontend"
@@ -71,7 +12,12 @@
     function clearSearchInput() {
       searchInput.value = '';
       searchResults.innerHTML = '';
+      nosearch.innerHTML = "No recent searches"
       nosearch.style.display = 'block';
+    }
+
+    function focusSearchInput() {
+        searchInput.focus();
     }
   
     searchInput.addEventListener('input', async (event) => {
@@ -89,7 +35,6 @@
           const decodedString = parser.parseFromString(text, 'text/html').body.textContent;
           return decodedString;
         }
-  
         console.log(hits)
 
         if(hits.length === 0){
@@ -106,16 +51,29 @@
 
           const cardBody = document.createElement('div');
           cardBody.classList.add('card-body');
+
+          cardBody.style.whiteSpace = 'normal'; // Allow text to wrap
+          cardBody.style.overflow = 'hidden'; // Hide any overflow
+          cardBody.style.textOverflow = 'ellipsis'; // Add ellipsis if text overflows
+          cardBody.style.width = '100%';
           
-          // const anchorLink = document.createElement('a');
           resultItem.href = hit.url;
-          cardBody.textContent = decodeHtmlEntities(hit._highlightResult.hierarchy.lvl1.value); 
+          // cardBody.textContent = decodeHtmlEntities(hit._highlightResult.hierarchy.lvl1.value); 
 
           
-          // cardBody.appendChild(anchorLink);
+          if (hit._snippetResult && hit._snippetResult.content) {
+            const highlightedContent = hit._snippetResult.content.value;
+            cardBody.innerHTML = highlightedContent;
+          } else {          
+            cardBody.textContent = decodeHtmlEntities(hit._highlightResult.hierarchy.lvl1.value); 
+          }
+
     
           resultItem.appendChild(cardBody);
-          
+
+          resultItem.addEventListener('click', () => {
+            modal.hide();
+          });
           searchResults.appendChild(resultItem);
         });
       } else {
@@ -126,10 +84,16 @@
         
       }
     });
+     
   
    
     const modal = new bootstrap.Modal(document.getElementById('searchBoxModal'));
     modal._element.addEventListener('hidden.bs.modal', clearSearchInput);
+
+     modal._element.addEventListener('shown.bs.modal', () => {
+      headerSearchBox.blur(); 
+      focusSearchInput();
+    });
   });
   
 
@@ -217,7 +181,7 @@ function getTagsForTOC(element) {
 const addLastChildOfMain = (allTags) => {
   //   console.log("inadd");
   let innerText = `
-        <div class="bs-scrollspy mt-3 mb-5 my-lg-0 mb-lg-5 px-sm-1 text-body-secondary"><span class="onThisTopic">ON THIS TOPIC</span>
+        <div class="bs-scrollspy mt-3 mb-5 my-lg-0 mb-lg-5 px-sm-1 text-body-secondary ignore-this"><span class="onThisTopic">ON THIS TOPIC</span>
         <nav>
         <div data-spy="scroll" data-target="#navbar-example3" data-offset="0" id="scrollTagDiv">
         `;
@@ -281,73 +245,104 @@ function setActiveClassOnClick(e) {
 }
 
 
-
-// ================================================== Comment Box
-// document.addEventListener("DOMContentLoaded", () => {
-//   const reviewButtons = document.querySelectorAll(".reviewBtn");
-//   const commentForm = document.querySelector(".commentForm");
-//   const comment__form = document.querySelector("#comment__form");
-//   const likeBtn = document.querySelector("#likeBtn");
-//   const dislikeBtn = document.querySelector("#dislikeBtn");
-
-//   reviewButtons.forEach((btn) => {
-//     btn.addEventListener("click", (event) => {
-//       event.preventDefault();
-//       const review = btn.name;
-//       if(review === "Liked") {
-//         likeBtn.classList.add("liked")
-//         dislikeBtn.classList.remove("disliked")
-//       }
-//       else if(review === "Disliked") {
-//         dislikeBtn.classList.add("disliked")
-//         likeBtn.classList.remove("liked")
-//       }
-//       const currentPageUrl = window.location.href;
-
-//       if (commentForm) {
-//         commentForm.classList.add("displayForm");
-//         const scrollOffset = 100; 
-//         window.scrollBy({
-//           top: commentForm.getBoundingClientRect().top - scrollOffset,
-//           behavior: "smooth",
-//         })
-//         for (const ele of comment__form.elements) {
-//             if (ele.name === "REVIEW") {
-//               ele.value = review;
-//             }
-//             else if (ele.name === "POST_URL") {
-//               ele.value = currentPageUrl;
-//             }
-//           }
-
-//       } else {
-//         console.error("commentForm not found.");
-//       }
-//     })
-//   })
-// });
+  // =============================================================== Comment form validation 
+  // (function () {
+  //   'use strict';
+  
+  //   const forms = document.querySelectorAll('.requires-validation');
+  //   const comment__form = document.querySelector("#comment__form");
+  
+  //   Array.from(forms).forEach(function (form) {
+  //     form.addEventListener('submit', function (event) {
+  //       if (!form.checkValidity()) {
+  //         event.preventDefault();
+  //         event.stopPropagation();
+  //       } else {
+  //         sendEmail(comment__form.elements, event);
+  //       }
+  
+  //       form.classList.add('was-validated');
+  //     }, false);
+  //   });
+  // })();
 
 
+  (function () {
+    'use strict';
+  
+    const forms = document.querySelectorAll('.requires-validation');
+    const comment__form = document.querySelector("#comment__form");
+    const commentForm = document.querySelector(".commentForm");
+    const thankYou = document.querySelector(".thankYou");
+  
+    function sendEmailWithLoader(formElements, event) {
+
+      const submitButton = formElements.submit;
+      submitButton.innerHTML = '<div class="spinner-border text-light" role="status"></div> Sending...';
+ 
+      event.preventDefault();
+      event.stopPropagation();
+  
+      setTimeout(function () {
+        console.log("Email sent!");
+        submitButton.innerHTML = 'Submit';
+        sendEmail(comment__form.elements, event);
+        console.log(comment__form.elements)
+        commentForm.classList.remove("displayForm")
+        thankYou.style.display = 'block';
+        
+      
+      }, 2000); 
+    }
+  
+    Array.from(forms).forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          sendEmailWithLoader(comment__form.elements, event);
+        }
+  
+        form.classList.add('was-validated');
+      }, false);
+    });
+  })();
+  
+  
+  
+
+// ================================================== Comment Box Hide show
   document.addEventListener("DOMContentLoaded", () => {
     const reviewButtons = document.querySelectorAll(".reviewBtn");
     const commentForm = document.querySelector(".commentForm");
     const comment__form = document.querySelector("#comment__form");
     const likeBtn = document.querySelector("#likeBtn");
     const dislikeBtn = document.querySelector("#dislikeBtn");
+    const thankYou = document.querySelector(".thankYou");
+    const emailField = document.querySelector("#EMAIL");
+    const commentField = document.querySelector("#DESCRIPTION");
+   
 
 
     reviewButtons.forEach((btn) => {
       btn.addEventListener("click", (event) => {
         event.preventDefault();
         event.stopPropagation();
+
+        emailField.value = ""
+        commentField.value = ""
         const review = btn.name;
+       
         if(review === "Liked") {
           likeBtn.classList.add("liked")
           dislikeBtn.classList.remove("disliked")
+          thankYou.style.display = 'none';
         }
         else if(review === "Disliked") {
-          dislikeBtn.classList.add("disliked")
+          dislikeBtn.classList.add("disliked")+
           likeBtn.classList.remove("liked")
+          thankYou.style.display = 'none';
         }
         const currentPageUrl = window.location.href;
 
@@ -367,10 +362,10 @@ function setActiveClassOnClick(e) {
               }
             }
 
-            let submitBtn = document.querySelector("#handleSubmit")
-            submitBtn.addEventListener("click" , () => {
-              sendEmail(comment__form.elements)
-            })
+            // let submitBtn = document.querySelector("#handleSubmit")
+            // submitBtn.addEventListener("click" , () => {
+            //   sendEmail(comment__form.elements)
+            // })
 
         } else {
           console.error("commentForm not found.");
@@ -379,7 +374,9 @@ function setActiveClassOnClick(e) {
     })
   });
 
-  function sendEmail(ele) {
+
+  function sendEmail(ele, event) {
+    event.preventDefault();
     Email.send({
       Host : "smtp.elasticemail.com",
       Username : "gurushesh.p@metapercept.com",
@@ -388,15 +385,157 @@ function setActiveClassOnClick(e) {
       From : `gurushesh.p@metapercept.com`,
       Subject : "User Feedback",
       Body : `User has submitted feedback for the following post <a href="${ele.POST_URL.value}"><b>Link</b></a> <br/>
-      <b>Email:</b> ${ele.EMAIL.value} <br/>
+      <b>Email:</b> ${ele.email.value} <br/>
       <b>User Reaction</b>: ${ele.REVIEW.value} <br/>
       <b>Comment</b>: ${ele.DESCRIPTION.value}
       `
   }).then(
-    message => alert(message)
+    message => console.warn(message)
+
   );
 
-  return alert("Feedback submitted..")
+  return console.warn("Feedback submitted..")
 }
 
 
+
+
+// ========================================================================= Print and Download Function 
+
+
+window.onload = function() {
+    document.getElementById('download-btn').addEventListener('click', function() {
+      const element = document.querySelector('article').cloneNode(true);
+      const elementsToIgnore = element.querySelectorAll('.ignore-this');
+      
+      elementsToIgnore.forEach(function(element) {
+        element.remove();
+      })
+
+    const imageTypes = ['png', 'jpeg', 'webp']; 
+    const options = {
+      margin: 10, // Optional: Adjust margin if needed
+      filename: 'page.pdf',
+      image: { type: imageTypes, quality: 0.98 }, 
+      html2canvas: { scale: 2 }, // Adjust scale if needed
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' } // Set A4 format
+    };
+
+    console.log(element)
+    html2pdf().from(element).set(options).save('page.pdf');
+  });
+}
+
+
+// ========================================================================= Working merged page code 
+// window.onload = function() {
+//   document.getElementById('download-btn').addEventListener('click', async function() {
+//     const mainElement = document.querySelector('article').cloneNode(true);
+//     const elementsToIgnore = mainElement.querySelectorAll('.ignore-this');
+      
+//     elementsToIgnore.forEach(function(element) {
+//       element.remove();
+//     });
+
+//     const imageTypes = ['png', 'jpeg', 'webp']; 
+//     const options = {
+//       margin: 10,
+//       filename: 'page.pdf',
+//       image: { type: imageTypes, quality: 0.98 }, 
+//       html2canvas: { scale: 2 },
+//       jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+//     };
+
+//     const linkedElements = mainElement.querySelectorAll('a');
+//     await generatePDFWithLinkedContent(linkedElements, mainElement, options);
+//   });
+// };
+
+// async function generatePDFWithLinkedContent(linkedElements, mainElement, options) {
+//   for (const linkElement of linkedElements) {
+//     const linkUrl = linkElement.getAttribute('href');
+//     if (linkUrl) {
+//       try {
+//         const response = await fetch(linkUrl);
+//         const linkedContent = await response.text();
+//         const linkedDocument = new DOMParser().parseFromString(linkedContent, 'text/html');
+//         const linkedArticle = linkedDocument.querySelector('article');
+        
+//         if (linkedArticle) {
+//           const linkedArticleClone = linkedArticle.cloneNode(true);
+//           const linkedElementsToIgnore = linkedArticleClone.querySelectorAll('.ignore-this');
+          
+//           linkedElementsToIgnore.forEach(function(element) {
+//             element.remove();
+//           });
+
+//           // Append the linked article content to the main content
+//           mainElement.appendChild(linkedArticleClone);
+//         }
+//       } catch (error) {
+//         console.error('Error fetching linked content:', error);
+//       }
+//     }
+//   }
+
+//   // Generate a PDF with all linked content and the main content
+//   html2pdf().from(mainElement).set(options).save('combined_articles.pdf');
+// }
+
+// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+
+
+
+// ========================================================================= Window.print function
+// function printPage() {
+//   // Add the no-print class to elements when printing
+//   var elementsToHide = document.querySelectorAll('.no-print');   //add this class name to those div's which you want to hide while printing current page 
+//   elementsToHide.forEach(function(element) {
+//     element.style.display = 'none';
+//   });
+
+//   // Trigger the browser's print dialog
+//   window.print();
+
+//   // Restore the display of hidden elements after printing
+//   elementsToHide.forEach(function(element) {
+//     element.style.display = 'block';
+//   });
+// }
+
+
+
+
+// ========================================================================= with header image
+
+// window.onload = function() {
+//   document.getElementById('download-btn').addEventListener('click', function() {
+//     const headerImage = new Image();
+//     headerImage.src = 'https://64ec5ff713ef6f7368f4b9e5--profound-croquembouche-1abdd5.netlify.app/images/Aurigo_logo.svg'; 
+
+//     const element = document.querySelector('article').cloneNode(true);
+//     const elementsToIgnore = element.querySelectorAll('.ignore-this');
+  
+//     elementsToIgnore.forEach(function(element) {
+//       element.remove();
+//     });
+
+//     const imageTypes = ['png', 'jpeg', 'webp']; 
+//     const options = {
+//       margin: 10, // Optional: Adjust margin if needed
+//       filename: 'page.pdf',
+//       image: { type: imageTypes, quality: 0.98 }, 
+//       html2canvas: { scale: 2 }, // Adjust scale if needed
+//       jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' } // Set A4 format
+//     };
+
+//     const combinedElement = document.createElement('div');
+//     combinedElement.appendChild(headerImage);
+//     combinedElement.appendChild(element);
+
+//     console.log(combinedElement);
+
+//     html2pdf().from(combinedElement).set(options).save('page.pdf');
+//   });
+// };
