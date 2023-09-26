58869157fd12d31fa8b4394dbc821a7439a7f3d6
Gurushesh-Metapercept <gurushesh.p@metapercept.com>
2023-09-26 14:49:54 +0530

    update

--- js/script.js
+++ js/script.js
@@ -320,7 +320,6 @@ function getTagsForTOC(element) {
         else if(parentDiv.id === ""){
           let tagText = element.innerText.replaceAll(" ", "_")
           const id = generateUniqueId();
-          console.log(tagText + id )
           element.id = tagText + id ;
         }
         nestedATags.push(element);
@@ -936,10 +935,10 @@ document.addEventListener("DOMContentLoaded", () => {
           const dot = title.length > 30 ? "..." : "" 
           previousBtn.innerHTML = `
           <a href="${preATagHref}" class="previous d-flex">
-            <div class="arrowIcon me-3 d-none d-md-block">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
-              </svg>
+            <div class="arrowIcon me-3 d-none d-md-flex d-md-justify-content-center d-md-align-items-center">
+              <div class="d-flex justify-content-center align-items-center">
+                <svg width="20" height="20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 13h8V7h-8V2l-8 8 8 8v-5z"/></svg>
+              </div>
             </div>
             <div class="btnText d-flex flex-column">
               <span>PREVIOUS</span>
@@ -959,11 +958,11 @@ document.addEventListener("DOMContentLoaded", () => {
              
              previousBtn.innerHTML = `
           <a href="${aa[index - 1].href}" class="previous d-flex">
-            <div class="arrowIcon me-3 d-none d-md-block">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
-              </svg>
+          <div class="arrowIcon me-3 d-none d-md-flex d-md-justify-content-center d-md-align-items-center">
+            <div class="d-flex justify-content-center align-items-center">
+              <svg width="20" height="20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 13h8V7h-8V2l-8 8 8 8v-5z"/></svg>
             </div>
+          </div>
             <div class="btnText d-flex flex-column">
               <span>PREVIOUS</span>
               ${elseArrLen.slice(0, 30)}${dot}
@@ -991,10 +990,10 @@ document.addEventListener("DOMContentLoaded", () => {
                   <span>NEXT</span>
                   ${title.slice(0, 30)}${dot}
                 </div>
-                <div class="arrowIcon ms-3 d-none d-md-block">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
-                  </svg>
+                <div class="arrowIcon ms-3 d-none d-md-flex d-md-justify-content-center d-md-align-items-center">
+                  <div class="d-flex justify-content-center align-items-center">
+                    <svg width="20" height="20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 7H2v6h8v5l8-8-8-8v5z"/></svg>
+                  </div>
                 </div>
             </a>
               `
@@ -1015,10 +1014,10 @@ document.addEventListener("DOMContentLoaded", () => {
                       <span>NEXT</span>
                       ${arrLen.slice(0, 30)}${dot}
                     </div>
-                    <div class="arrowIcon ms-3 d-none d-md-block">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-right-short" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"/>
-                      </svg>
+                    <div class="arrowIcon ms-3 d-none d-md-flex d-md-justify-content-center d-md-align-items-center">
+                      <div class="d-flex justify-content-center align-items-center">
+                        <svg width="20" height="20" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 7H2v6h8v5l8-8-8-8v5z"/></svg>
+                      </div>
                     </div>
                 </a>
                   `
